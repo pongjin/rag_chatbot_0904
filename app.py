707fe379575888da39dcf7ca8188d9b16e23cfd6
pong_jin <106025091+pongjin@users.noreply.github.com>
2025-09-15 18:14:47 +0900

    Update app.py

--- app.py
+++ app.py
@@ -275,15 +275,14 @@ def main():
                             f.write(r.content)
                     
                     # 파일 크기 확인 (정상적으로 받았는지 체크)
-                    st.text(os.path.getsize(font_path))
+                    st.text("주로 언급된 키워드")
                     
-                    text = "안녕하세요 스트림릿 클라우드에서 한글 워드클라우드 테스트 중입니다."
                     wc = WordCloud(
                         font_path=font_path, 
                         background_color="white", 
                         width=800, 
                         height=400
-                    ).generate(text)
+                    ).generate_from_frequencies(dict(zip(top10['keyword'], top10['user_id'])))
                     
                     # 시각화
                     fig, ax = plt.subplots(figsize=(10, 5))
